2536ff406a8914717db5a865547eeac6ac54b88d
LucasG1122 <137423272+LucasG1122@users.noreply.github.com>
2025-02-24 16:45:16 -0500

    Scoring and Multiplier

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class NoteObject : MonoBehaviour {
 
     public bool canBePressed;
+    // Add a flag to indicate if the note has been hit already
+    private bool hasBeenHit = false;
 
     public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
 
     // Use this for initialization
     void Start () {
@@ -16,12 +20,33 @@ public class NoteObject : MonoBehaviour {
     void Update () {
         if(Input.GetKeyDown(keyToPress))
         {
-            if(canBePressed)
+            if(canBePressed && !hasBeenHit)
             {
+                // Mark note as hit so that we don't register a miss later.
+                hasBeenHit = true;
                 gameObject.SetActive(false);
 
-                GameManager.instance.NoteHit();
-            }
+                // Determine timing and trigger the appropriate hit
+                float absY = Mathf.Abs(transform.position.y);
+                if(absY > 0.25f)
+                {
+                    Debug.Log("Hit");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                } 
+                else if(absY > 0.05f)
+                {
+                    Debug.Log("Good");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                } 
+                else
+                {
+                    Debug.Log("Perfect");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
+            } 
         }
     }
 
@@ -38,8 +63,12 @@ public class NoteObject : MonoBehaviour {
         if(other.tag == "Activator")
         {
             canBePressed = false;
-
-            GameManager.instance.NoteMissed();
+            // Only register a miss if the note wasn't hit already.
+            if(!hasBeenHit)
+            {
+                GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+            }
         }
     }
 }
